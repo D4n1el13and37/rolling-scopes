96cd8b988cdaceb1cc81248af0f7f273a4a5589e
D4n1el13and37 <daniel837@mail.ru>
2024-07-13 00:09:59 +0300

    feat: add outlet, add link to detail page(wrap card into), get id from planet url

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PlanetWithImage } from '../../api/apiMethods';
 import Card from '../card/Card';
 import './Content.css';
+import { Link, Outlet } from 'react-router-dom';
 
 interface ContentProps {
   planets: PlanetWithImage[];
@@ -12,23 +13,22 @@ const Content: React.FC<ContentProps> = ({ planets }) => {
     <div className="content__wrapper">
       <div className="planets__list">
         {planets.map((planet: PlanetWithImage, index) => {
+          const id = planet.url.split('/').slice(-2, -1)[0];
+
           return (
-            <Card
-              key={planet.name + index}
-              imageUrl={planet.imageUrl}
-              name={planet.name}
-              population={planet.population}
-              climate={planet.climate}
-              terrain={planet.terrain}
-            />
+            <Link key={planet.name + index} to={`card/${id}`}>
+              <Card key={planet.name + index} {...planet} />
+            </Link>
           );
         })}
+
         {!planets.length && (
           <div className="plenets__notfound">
             It seems that such a planet is not on our list
           </div>
         )}
       </div>
+      <Outlet />
     </div>
   );
 };
