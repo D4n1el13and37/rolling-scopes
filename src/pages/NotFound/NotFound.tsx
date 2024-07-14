2c0bad63e7ddba71b09a63a469a888bb4493c662
D4n1el13and37 <daniel837@mail.ru>
2024-07-14 12:44:27 +0300

    feat: add go back button

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Button from '../../components/ui-kit/Button';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="app__error_wrapper">
         <h1 className="app__error_title">Seems we've lost our way</h1>
-        <Link to="/">
-          <Button className="app__error_button">Return on the way</Button>
-        </Link>
+        <Button onClick={() => navigate('/')} className="app__error_button">
+          Return on the way
+        </Button>
       </div>
     </div>
   );
